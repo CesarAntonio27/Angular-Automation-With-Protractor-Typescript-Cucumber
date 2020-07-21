ceff631db4371e7a387bfad6ec123c5bb9b22ec0
cesar <antonioc@titaniumsolutions.com.mx>
2020-07-21 12:36:48 -0500

    modificaciones en mi proyecto durante la clase 25

--- Test/feature/Home.feature
+++ Test/feature/Home.feature
@@ -3,8 +3,13 @@ Feature: To work with home page
 
 @smoke
 Scenario: click course of application
+    And I get all the heading 
+    And I click the 'selenium framework development' course 
+    Then I should see 'Selenium framework development' course in coursedetails page
+    
 
-    Given I navigate to application
+@smoke
+Scenario: click course of application in second time
     And I get all the heading 
     And I click the 'selenium framework development' course 
     Then I should see 'Selenium framework development' course in coursedetails page
--- Test/hook/ScenarioHook.ts
+++ Test/hook/ScenarioHook.ts
@@ -1,10 +1,24 @@
-import {defineSupportCode} from 'cucumber'
+import {defineSupportCode } from 'cucumber'
+import { browser } from 'protractor';
+import { config } from '../Steps/config';
 
 
-defineSupportCode(({registerHandler}) =>{
+defineSupportCode(({ registerHandler }) =>{
     
     registerHandler('BeforeFeature', async()=>{
         console.log('Executing before feature !!');
+    });
+
+    registerHandler('BeforeScenario', async()=>{
+        await browser.get(config.baseUrl);
+    });
+    
+    registerHandler('AfterStep', async()=>{
+        console.log('Step executed !!');
+    });
+
+    registerHandler('AfterScenario', async()=>{
+        console.log('scenario executed !!');
     });
 
     registerHandler('AfterFeature', async()=>{
